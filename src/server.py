5e1df6302ffaeb8b57a2683ee7240810d7df0151
Meng-Chieh Ling <m.ling@cosnova.com>
2025-03-19 10:12:13 +0100

    update

--- src/server.py
+++ src/server.py
@@ -1,21 +1,25 @@
 import logging
 import sys
 import os
+from operator import itemgetter
 
 import uvicorn
 from fastapi import FastAPI
 from langserve import add_routes
-from langchain.chat_models import ChatOpenAI
-from langchain_core.runnables import chain
+from langchain_openai import ChatOpenAI
+from langchain_core.runnables import RunnablePassthrough, chain
+from langchain_core.messages import AIMessage
+
 
 from src.initialization import credential_init
+from src.trendweek_report.deep_search import OpenAIWebSearch
+from src.trendweek_report.examples_with_deep_search import ExampleSearch, response_to_output_text
 
 logging.basicConfig(stream=sys.stdout, level=logging.INFO)
 logger = logging.getLogger(__name__)
 
 
 credential_init()
-
 
 model = ChatOpenAI(openai_api_key=os.environ['OPENAI_API_KEY'],
                    model_name="gpt-4o-2024-05-13", temperature=0)
@@ -30,6 +34,25 @@ add_routes(
     app,
     model,
     path="/openai",
+)
+
+############################################################################################
+
+@chain
+def final_output_2_ai_message(input_):
+
+    return AIMessage(content=input_['final_output']['result'])
+
+
+example_search = ExampleSearch(model='gpt-4o-mini')
+openai_websearch = OpenAIWebSearch(model="gpt-4o", temperature=0)
+
+pipeline_ = {'question': example_search.prompt_pipeline}|openai_websearch.pipeline|RunnablePassthrough.assign(final_output=itemgetter("context")|response_to_output_text|example_search.parsing_pipeline)|final_output_2_ai_message
+
+add_routes(
+    app,
+    pipeline_,
+    path="/openai_websearch",
 )
 
 if __name__ == '__main__':
---
+++ src/timeseries/TFT_forecaster.py
@@ -0,0 +1,312 @@
+import os
+import logging
+import warnings
+from argparse import Namespace
+from typing import Optional
+
+import pandas as pd
+from pytorch_forecasting import TemporalFusionTransformer, TimeSeriesDataSet, QuantileLoss
+from pytorch_forecasting.models.temporal_fusion_transformer.tuning import optimize_hyperparameters
+from pytorch_forecasting.models.base_model import Prediction
+
+from src import timeseries as ts
+from src.io.path_definition import get_datafetch
+from src.timeseries.base_forecaster import BaseForecaster
+
+warnings.filterwarnings("ignore")
+
+# Configure logging
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+
+class TFTForecaster(BaseForecaster):
+    """Class for training and making predictions using Temporal Fusion Transformer."""
+
+    MODEL_FOLDER = os.path.join(get_datafetch(), 'model', 'TFT')
+    NAME = 'TFT'
+
+    def __init__(self, args: Namespace, filename: Optional[str]=None):
+
+        ts.OPTIMIZER_TYPE = args.optimizer # Define optimizer as a constant for clarity
+        ts.DEFAULT_MAX_EPOCHS = args.max_epochs
+        ts.MAX_PREDICTION_LENGTH = args.max_prediction_length
+        ts.MAX_ENCODER_LENGTH = args.max_encoder_length
+        ts.BATCH_SIZE = args.batch_size
+        ts.GROUP_IDS = args.group_ids
+        ts.TIME_VARYING_UNKNOWN_REALS = [ts.TARGET]
+
+        super().__init__(filename=filename)  # Call the BaseForecaster constructor if needed
+
+    def fit(self, df: pd.DataFrame, validation: bool, **kwargs):
+        """Fits the Temporal Fusion Transformer model.
+
+        Args:
+            df (pd.DataFrame): Training dataset.
+            validation (bool): Whether to use validation data.
+            **kwargs: Additional hyperparameters for model training.
+        """
+        model_filename = os.path.join(self.MODEL_FOLDER, f'{self._filename}.ckpt')
+
+        if os.path.isfile(model_filename):
+            os.remove(model_filename)
+
+        assert ts.TIME_IDX in df.columns, f"{ts.TIME_IDX} does not exist in df"
+        assert ts.TARGET in df.columns, f"{ts.TARGET} does not exist in df"
+
+        self._learning_rate = kwargs.get('learning_rate', 0.03)
+        self._hidden_size = kwargs.get('hidden_size', 16)
+        self._dropout = kwargs.get("dropout", 0.1)
+        self._gradient_clip_val = kwargs.get("gradient_clip_val", 0.1)
+        self._attention_head_size = kwargs.get("attention_head_size", 2)
+        self._hidden_continuous_size = kwargs.get("hidden_continuous_size", 8)
+
+        # Call parent `fit` method to handle preprocessing
+        self._assert(df, validation)
+
+        self._pipeline()
+
+    def _pipeline(self):
+        """Executes the end-to-end pipeline for model training.
+
+        This function:
+            - Loads the training configuration.
+            - Builds training and validation dataloaders.
+            - Initializes the PyTorch Lightning Trainer.
+            - Builds the Temporal Fusion Transformer (TFT) model.
+            - Starts the model training process.
+            - Determines the optimized number of epochs if not pre-defined.
+
+        Logs:
+            - Pipeline execution steps for better debugging and monitoring.
+
+        Returns:
+            None: This method does not return anything; it sets up and trains the model.
+        """
+        logger.info("Starting pipeline execution.")
+
+        # Load training configuration
+        self._load_training_config()
+        logger.info("Training configuration loaded.")
+
+        # Build train and validation dataloaders
+        if self._validation:
+            train_dataloaders, val_dataloaders = self._build_train_val_dataloader(self._training, self._df,
+                                                                                  predict=False)
+        else:
+            train_dataloaders = self._training.to_dataloader(train=True, batch_size=ts.BATCH_SIZE, num_workers=0)
+            val_dataloaders = None
+
+        logger.info("Train and validation dataloaders created.")
+
+        # Initialize Trainer
+        self._build_trainer()
+        logger.info("Trainer initialized.")
+
+        # Build TFT model
+        self._build_model()
+        logger.info("TFT model is built.")
+
+        # Train the model
+        logger.info("Starting model training...")
+        self._trainer.fit(self._model, train_dataloaders=train_dataloaders, val_dataloaders=val_dataloaders)
+        logger.info("Model training completed.")
+
+        self._get_optimized_epoch()
+
+        logger.info("Pipeline execution completed successfully.")
+
+    def _load_training_config(self):
+        """Loads and configures the training dataset.
+
+        This method:
+        - Ensures the required columns exist in `self._df`.
+        - Converts `ts.TIME_IDX` to integers for consistency.
+        - Defines a training dataset using `TimeSeriesDataSet`.
+
+        Raises:
+            AssertionError: If any required column is missing in `self._df`.
+            KeyError: If `ts.TIME_IDX` column is missing while trying to convert its type.
+        """
+
+        required_columns = {"brand", "retailer", ts.TIME_IDX}
+        missing_columns = required_columns - set(self._df.columns)
+
+        if missing_columns:
+            raise ValueError(f"Missing required columns in self._df: {missing_columns}")
+
+        self._df[ts.TIME_IDX] = self._df[ts.TIME_IDX].astype(int)
+
+        if self._validation:
+            cutoff = self._df[ts.TIME_IDX].max() - ts.MAX_PREDICTION_LENGTH
+        else:
+            cutoff = self._df[ts.TIME_IDX].max()
+
+        self._training = TimeSeriesDataSet(
+            self._df.loc[lambda x: x.time_idx <= cutoff],
+            time_idx=ts.TIME_IDX,
+            target=ts.TARGET,
+            group_ids=ts.GROUP_IDS,
+            max_encoder_length=ts.MAX_ENCODER_LENGTH,
+            max_prediction_length=ts.MAX_PREDICTION_LENGTH,
+            time_varying_unknown_reals=ts.TIME_VARYING_UNKNOWN_REALS,
+            # categorical_encoders=ts.CATEGORICAL_ENCODERS,
+        )
+
+    def predict(self, df: pd.DataFrame, filename: Optional[str]=None, plot: bool=False) -> pd.DataFrame:
+        """Makes predictions using the trained Temporal Fusion Transformer model.
+
+        Args:
+            df (pd.DataFrame): Input dataframe for prediction.
+
+        Returns:
+            pd.DataFrame: Dataframe containing predictions.
+        """
+
+        if filename is None:
+            model_filename = os.path.join(self.MODEL_FOLDER, f'best_model.ckpt')
+        else:
+            model_filename = filename
+        assert os.path.isfile(model_filename), f"{model_filename} does not exist."
+        self._load_best_model(model_filename)
+
+        self._df = df
+        self._load_training_config()
+        _, predict_dataloader = self._build_train_val_dataloader(self._training, df, predict=True)
+
+        predictions = self.best_model.predict(predict_dataloader,
+                                              trainer_kwargs=dict(accelerator="cpu"),
+                                              return_index=True)
+
+        return self.prediction2dataframe(predictions).dropna(subset=['y_pred'])
+
+    def _load_best_model(self, filename: str):
+        """Loads the best trained model from checkpoint.
+
+        Args:
+            filename (str): Path to the model checkpoint.
+        """
+        self.best_model = TemporalFusionTransformer.load_from_checkpoint(filename)
+
+    @staticmethod
+    def prediction2dataframe(predictions: Prediction) -> pd.DataFrame:
+        """Converts predictions into a structured dataframe.
+
+        Args:
+            predictions (Prediction): Model predictions.
+
+        Returns:
+            pd.DataFrame: Dataframe containing structured predictions.
+        """
+        data = []
+
+        for idx, row in predictions.index.iterrows():
+            time_idx = row[ts.TIME_IDX]
+            brand = row['brand']
+            retailer = row['retailer']
+            prediction = predictions[0][idx]
+            for idy, pred in enumerate(prediction):
+                data.append([brand, retailer, time_idx + idy, float(pred)])
+
+        return pd.DataFrame(data, columns=['brand', 'retailer', ts.TIME_IDX, 'y_pred'])
+
+    def _build_model(self):
+
+        net = TemporalFusionTransformer.from_dataset(
+            self._training,
+            learning_rate=self._learning_rate,
+            hidden_size=self._hidden_size,  # most important hyperparameter apart from learning rate
+            attention_head_size=self._attention_head_size,
+            dropout=self._dropout,  # between 0.1 and 0.3 are good values
+            hidden_continuous_size=self._hidden_size,  # set to <= hidden_size
+            optimizer=ts.OPTIMIZER_TYPE,
+            loss=QuantileLoss()
+        )
+
+        logger.info(f"Number of parameters in network: {net.size() / 1e3:.1f}k")
+
+        self._model = net
+
+    def optimize_hyperparameters(self, df: pd.DataFrame, n_trials: int = 20):
+
+        self._validation = True
+
+        self._df = df
+        self._load_training_config()
+        train_dataloader, val_dataloader = self._build_train_val_dataloader(self._training, self._df, predict=False)
+        study = optimize_hyperparameters(
+            train_dataloader,
+            val_dataloader,
+            model_path="optuna_TFT",
+            n_trials=n_trials,
+            max_epochs=20,
+            gradient_clip_val_range=(0.01, 1.0),
+            hidden_size_range=(8, 128),
+            hidden_continuous_size_range=(8, 128),
+            attention_head_size_range=(1, 4),
+            learning_rate_range=(0.001, 0.1),
+            dropout_range=(0.1, 0.3),
+            # trainer_kwargs=dict(limit_train_batches=30),
+            reduce_on_plateau_patience=4,
+            use_learning_rate_finder=False,
+            # use Optuna to find ideal learning rate or use in-built learning rate finder
+        )
+
+        self._save_hyperparameters(study)
+
+
+if __name__ == "__main__":
+
+    import argparse
+
+    from datetime import datetime
+
+    from src.io.path_definition import get_project_dir
+
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument('--optimizer', type=str, default='AdamW', help="The neural network optimizer")
+    parser.add_argument('--max_epochs', type=int, default='20')
+    parser.add_argument('--max_prediction_length', type=int, default=12)
+    parser.add_argument('--max_encoder_length', type=int, default=36)
+    parser.add_argument('--batch_size', type=int, default=16)
+    parser.add_argument('--group_ids', type=str, nargs='+')
+
+    args = parser.parse_args()
+
+    filename = os.path.join(get_project_dir(), 'src', 'timeseries', 'sellout_experiement.csv')
+
+    df = pd.read_csv(filename, index_col=0, dtype={"quantity": float})
+
+    df = df[(df['country_iso'] == 'DE')]
+    df_agg = df.groupby(['retailer', "month", "year", "brand"]).agg(y=("quantity", "sum"))
+    df_agg.reset_index(inplace=True)
+
+    df_agg['time'] = df_agg[['year', 'month']].apply(lambda x: datetime.strptime(f"{x[0]}-{x[1]}", "%Y-%m"),
+                                                     axis=1)
+
+    df_agg[ts.TIME_IDX] = df_agg[['year', 'month']].apply(lambda x: 12 * x[0] + x[1], axis=1)
+    df_agg[ts.TIME_IDX] = df_agg[ts.TIME_IDX] - df_agg[ts.TIME_IDX].min()
+    df_agg[ts.TIME_IDX] = df_agg[ts.TIME_IDX].astype(int)
+
+    df_agg = df_agg[df_agg['brand'].isin(['Catrice', 'essence'])]
+
+    tft = TFTForecaster(args=args)
+
+    # train-test split
+    cutoff = df_agg[ts.TIME_IDX].max() - ts.MAX_PREDICTION_LENGTH
+
+    df_train = df_agg[df_agg[ts.TIME_IDX] <= cutoff]
+
+    tft.optimize_hyperparameters(df=df_train, n_trials=5)
+
+    params = tft.load_hyperparameters()
+
+    tft.fit(df=df_train, validation=True, **params)
+    tft.fit(df=df_train, validation=False, **params)
+
+    df_prediction = tft.predict(df=df_agg, plot=True)
+
+    final_df = pd.merge(df_agg, df_prediction, on=[ts.TIME_IDX, 'retailer', 'brand'], how='left')
+
+    final_df.to_csv(os.path.join(get_datafetch(), "sellout_forecast_TFT.csv"))
